3ac56cfbb0e63b82e57f9b0256e8464d878190c9
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2022-08-29 14:14:13 -0300

    modifique el txt de apis

--- TeoriaJavascript/Apis/apis.js
+++ TeoriaJavascript/Apis/apis.js
@@ -1,3 +1,4 @@
+//llamada a la API con fetch
 fetch("url")
   .then((respuesta) => respuesta.json()) // esta respuesta es la api consumida pero en formato json (tenes que pasarla por la funcion json() obtener los datos manejables)
   .then((data) => console.log(data)) // data es la info manejable que la pasas al array para poder trabajarlo normalmente
