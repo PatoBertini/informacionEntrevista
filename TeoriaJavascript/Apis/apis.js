0e762960546f2531ca6f892e52f708839fbb06f5
Patricio Bertini <patobertini10@gmail.com>
2022-08-30 08:37:04 +0200

    get data from API

--- TeoriaJavascript/Apis/apis.js
+++ TeoriaJavascript/Apis/apis.js
@@ -1,6 +1,17 @@
-fetch("url")
-  .then((respuesta) => respuesta.json()) // esta respuesta es la api consumida pero en formato json (tenes que pasarla por la funcion json() obtener los datos manejables)
-  .then((data) => console.log(data)) // data es la info manejable que la pasas al array para poder trabajarlo normalmente
+const array = [];
+// Metodo Fetch es una forma mas nueva y simple de cumplir la misma funcion que Ajax. Esta basado en promesas, siempre nos devuelve una promesa encapsulada y hay que convertirla a un tipo de dato valido.
+
+// Metodo GET = obtenemos info de una api
+fetch("https://fakestoreapi.com/products")
+  .then((dataRecibida) => dataRecibida.json())
+
+  // esta respuesta es la api consumida pero en formato json, es una promesa encapsulada. (tenes que pasarla a cadena de texto por la funcion json() obtener los datos manejables)
+  .then((data) => {
+    array.push(data);
+    console.log(array);
+  }) // data es la info manejable (formato texto) que la pasas al array para poder trabajarlo normalmente
   .catch((error) => {
     console.log("salio todo mal", error);
-  });
+  })
+  .finally(() => console.log("Me ejecuto siempre, soy .finally"));
+
